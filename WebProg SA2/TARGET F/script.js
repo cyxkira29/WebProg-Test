5bb8c727097e1a896a37cfa820c8ede1acb86e45
Cyrus Yleaña <kaguyamasan1@gmail.com>
2025-02-01 18:34:47 +0800

    Add files via upload

---
+++ WebProg SA2/TARGET F/script.js
@@ -0,0 +1,7 @@
+const buttons = document.querySelectorAll(".view-grade-btn");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    alert("Grade details coming soon!");
+  });
+});
